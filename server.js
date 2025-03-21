158b96c724e1ca4ab9100a95f42015c4152da627
Jenner894 <jenner88@protonmail.com>
2025-03-21 21:15:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,17 @@ OrderSchema.post('save', async function(doc) {
         { $addToSet: { orders: doc._id } }
     );
 });
-
+// Méthode pour régénérer la clé Telegram
+UserSchema.methods.regenerateKeys = function() {
+    // Générer une nouvelle clé de 16 caractères
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let newKey = '';
+    for (let i = 0; i < 16; i++) {
+        newKey += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    this.keys = newKey;
+    return this.save();
+};
 // Modification du middleware post-save de Order pour mettre à jour les statistiques de l'utilisateur
 OrderSchema.post('save', async function(doc) {
     try {
