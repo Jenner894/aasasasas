bf6f0890fb378849dfeb89560a858bfb78765955
Jenner894 <jenner88@protonmail.com>
2025-03-22 17:07:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production', // S'assurer que c'est false en développement
+    secure: process.env.NODE_ENV === 'production', // Doit être false en développement local
     httpOnly: true,
     sameSite: 'lax',
     maxAge: 24 * 60 * 60 * 1000 // 24 heures
